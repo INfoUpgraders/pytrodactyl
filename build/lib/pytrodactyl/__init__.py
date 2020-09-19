a72667bd9d0c1f93b68b2a8be9e0ca21d0f9628a
infoupgraders <infoupgraders@gmail.com>
2020-09-18 20:13:43 -0400

    d

---
+++ build/lib/pytrodactyl/__init__.py
@@ -0,0 +1,7 @@
+import requests
+
+class Server:
+    def __init__(self, url:str, text:bool=True, plain:bool=False):
+        self.url = url
+        self.text = text
+        self.plain = plain
--- pytrodactyl/__init__.py
+++ pytrodactyl/__init__.py
@@ -1,7 +1,27 @@
 import requests
+import json
 
-class Server:
-    def __init__(self, url:str, text:bool=True, plain:bool=False):
-        self.url = url
-        self.text = text
-        self.plain = plain
+class Client:
+	def __init__(self, url, api_key):
+		self.url = url
+		self.api_key = api_key
+
+	class Application:
+		def __init__(self):
+			self.headers = {
+			  'Accept': 'Application/vnd.pterodactyl.v1+json',
+			  'Content-Type': 'application/json',
+			  'Authorization': f'Bearer {self.api_key}'
+			}
+
+		def _api_request(url, headers=self.headers)
+			response = requests.get(url=url, headers=headers)
+			return response
+
+		def show_all_servers(self):
+			meta_data = _api_request(url=f"{self.url}/api/application/servers")
+			response = _api_request(url=f"{self.url}/api/application/servers?page={num}")
+			b = []
+			for i in range(meta_data['meta']['pagination']['total_pages']):
+				b.append(page(i))
+			return b
