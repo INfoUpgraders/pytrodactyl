bb92bf1a13f1e61cc1b96443eff0dc954f034241
infoupgraders <infoupgraders@gmail.com>
2020-09-18 21:25:18 -0400

    Added 'show all servers' method.

--- pytrodactyl/__init__.py
+++ pytrodactyl/__init__.py
@@ -1,27 +1,29 @@
 import requests
 import json
+import sys
 
-class Client:
+
+class Application:
 	def __init__(self, url, api_key):
 		self.url = url
 		self.api_key = api_key
+		self.headers = {
+		  'Accept': 'Application/vnd.pterodactyl.v1+json',
+		  'Content-Type': 'application/json',
+		  'Authorization': f'Bearer {self.api_key}'
+		}
+		response = requests.get(url=self.url + "/api/application/servers", headers=self.headers)
+		if response.status_code != 200:
+			print(response.json())
+			sys.exit()
 
-	class Application:
-		def __init__(self):
-			self.headers = {
-			  'Accept': 'Application/vnd.pterodactyl.v1+json',
-			  'Content-Type': 'application/json',
-			  'Authorization': f'Bearer {self.api_key}'
-			}
+	def _api_request(self, url):
+		response = requests.get(url=url, headers=self.headers)
+		return response.json()
 
-		def _api_request(url, headers=self.headers)
-			response = requests.get(url=url, headers=headers)
-			return response
-
-		def show_all_servers(self):
-			meta_data = _api_request(url=f"{self.url}/api/application/servers")
-			response = _api_request(url=f"{self.url}/api/application/servers?page={num}")
-			b = []
-			for i in range(meta_data['meta']['pagination']['total_pages']):
-				b.append(page(i))
-			return b
+	def show_all_servers(self):
+		meta_data = self._api_request(url=f"{self.url}/api/application/servers")
+		b = []
+		for i in range(meta_data['meta']['pagination']['total_pages']):
+			b.append(self._api_request(url=f"{self.url}/api/application/servers?page={i}"))
+		return b
