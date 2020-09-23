4111d98c42a3e6b0f883b28dd29186829f66d73c
INfoUpgraders <infoupgraders@gmail.com>
2020-09-23 10:47:29 -0400

    updated

--- test.py
+++
@@ -1,6 +0,0 @@
-from pytrodactyl import Application
-import time
-
-app = Application(url="https://panel.fatcat.bike", api_key="Ni9Xd7Yt26A2yxtiMjHmC1rYdMKHEyLfc57q8p6wtSKl20RS")
-
-print(app.show_all_servers())
